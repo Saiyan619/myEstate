fe31cea4c1d8f77575db13939f767a7dbeb44ea2
olaniyi <arokoyueb11@gmail.com>
2025-01-22 23:52:06 +0100

    integrating backend apis

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import { useState } from 'react';
 import Hero from './components/Hero';
 import Nav from '../../GlobalComponents/Nav';
 import HouseCard from './components/HouseCard';
 import GlobalApi from '../../Utils/GlobalApi';
 
 const HomePage = () => {
+  const [allHouses, setAllHouses] = useState([])
 
   const getHouseAll = async () => {
     try {
       const response = await GlobalApi.getAllHouse()
       console.log(response.data)
+      setAllHouses(response.data)
     } catch (error) {
       console.error(error)
     }
@@ -21,7 +24,11 @@ const HomePage = () => {
       <Hero />
 <button onClick={getHouseAll}>get house test</button>
       <h2 className='text-4xl font-bold text-center'>Find your Dream house Here</h2>
-        <HouseCard />
+
+      {allHouses.map((item, index) => {
+        return <HouseCard key={index} item={item} />
+      })}
+       
     </div>
   );
 };
