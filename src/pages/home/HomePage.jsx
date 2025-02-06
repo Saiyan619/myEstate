51b310c06914a37c4b30e0f0599a53fbe997e195
olaniyi <arokoyueb11@gmail.com>
2025-02-06 19:48:08 +0100

    building frontend

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -4,6 +4,7 @@ import Hero from './components/Hero';
 import Nav from '../../GlobalComponents/Nav';
 import HouseCard from './components/HouseCard';
 import GlobalApi from '../../Utils/GlobalApi';
+import HomeSectionOne from './homepageSections/homeSectionOne';
 
 const HomePage = () => {
   const [allHouses, setAllHouses] = useState([])
@@ -29,12 +30,26 @@ const HomePage = () => {
       <Hero />
       <h2 className='text-4xl font-bold text-center'>Find your Dream house Here</h2>
 
+      <div className='flex flex-col items-center pb-10'>
       <div className='flex gap-4 items-center justify-center'>
       {allHouses.map((item, index) => {
         return <HouseCard key={index} item={item} />
       })}
+
+
       </div>
-       
+
+      <div className='text-center mt-10'>
+        <button className='btn btn-primary'>Browse more Properties</button>
+      </div>
+     </div>
+
+    
+
+      
+      <HomeSectionOne />
+
+
     </div>
   );
 };
---
+++ src/pages/home/homepageSections/HomeSectionOne.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+
+const HomeSectionOne = () => {
+  return (
+    <div className='bg-blue-950 text-white pt-20'>
+          <div className='flex items-center justify-center gap-64'>
+              <h3 className='text-4xl font-semibold '>We make it easy for <br /> <span className='text-purple-700 '>tenants</span> and <span className='text-purple-700 '>landlord</span></h3>
+              <p className='text-gray-200 text-sm'>
+                  Wheter its selling your current home, getting <br /> financing or buying a new home, we make it easy and <br /> effienct.
+                  The best part? Youll save a bunch of money <br /> and time with our services.
+              </p>
+      </div>
+
+          {/* <div className='flex items-center justify-center gap-64'>
+          <div className="card bg-primary text-primary-content w-96">
+  <div className="card-body">
+    <h2 className="card-title">Card title!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn">Buy Now</button>
+    </div>
+  </div>
+              </div>
+
+              <div className="card bg-primary text-primary-content w-96">
+  <div className="card-body">
+    <h2 className="card-title">Card title!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn">Buy Now</button>
+    </div>
+  </div>
+</div>
+          </div> */}
+
+          <div className="flex items-center justify-center mt-32 flex-col">
+              <span className='text-2xl font-bold'>Our Numbers Dont Lie</span>
+          <div className="stats bg-transparent text-white mt-12">
+  <div className="stat place-items-center">
+    <div className="stat-value">7.4%</div>
+    <div className="stat-desc text-gray-400">↗︎ Property Return Value</div>
+  </div>
+
+  <div className="stat place-items-center">
+    <div className="stat-value">4,200</div>
+    <div className="stat-desc text-gray-400">↗︎ Propety in Sell & Rent</div>
+  </div>
+
+  <div className="stat place-items-center">
+    <div className="stat-value">1,200</div>
+    <div className="stat-desc text-gray-400">↗ LandLords Connected</div>
+  </div>
+</div>
+          </div>
+          
+
+          <div className="hero bg-white min-h-1/2">
+  <div className="hero-content text-center flex items-center justify-center">
+                  <div className="max-w-md">
+                      <span className='text-purple-700'>No spam promise</span>
+      <h1 className="text-4xl font-bold text-black">Are you a LandLord?</h1>
+      <p className=" text-gray-500">
+        Discover ways to increase your home value and get homelisted. No spam
+                      </p>
+                      <p className='text-gray-400'>Join +10,000 other Landlord in our estate community</p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
+      
+    </div>
+  )
+}
+
+export default HomeSectionOne
