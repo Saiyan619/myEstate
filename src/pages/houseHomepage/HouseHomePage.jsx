2cb4229b90d1396ffa6e689c6fd33991b003e157
olaniyi <arokoyueb11@gmail.com>
2025-03-02 07:36:19 +0100

    building Ui for homepage for houses

--- src/pages/houseHomepage/HouseHomePage.jsx
+++ src/pages/houseHomepage/HouseHomePage.jsx
@@ -1,23 +1,40 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import GlobalApi from '../../Utils/GlobalApi'
+import HouseCard from '../home/components/HouseCard'
+import SkeletonHomeCard from '../home/components/SkeletonHomeCard'
 
 const HouseHomePage = () => {
 
+      const [allHouses, setAllHouses] = useState([])
+    
     const getHouseAll = async () => {
-        try {
-            const response = await GlobalApi.getAllHouse()
-            console.log(response.data)
-        } catch (error) {
-            console.error(error)
-        }
+      try {
+        const response = await GlobalApi.getAllHouse()
+        console.log(response.data)
+        setAllHouses(response.data)
+      } catch (error) {
+        console.error(error)
+      }
     }
-
   return (
       <div>
           <button className='btn'onClick={getHouseAll}>test api</button>
-      HouseHomePage
-      HouseHomePage
+          {!allHouses || allHouses.length === 0 ?
+           (  <div className='flex gap-4 items-center flex-wrap justify-center'>
+            <SkeletonHomeCard />
+            <SkeletonHomeCard />
+            <SkeletonHomeCard />
+          </div>)
+        :
+
+(<div className='flex gap-4 items-center flex-wrap justify-center'>
+  {allHouses.map((item, index) => {
+    return <HouseCard key={index} item={item} />
+  })}
+    </div>)
+      }
+    
     </div>
   )
 }
