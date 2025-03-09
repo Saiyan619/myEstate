296be835f1ba2c2b534e337a948228bf5841e06f
olaniyi <arokoyueb11@gmail.com>
2025-03-09 11:38:53 +0100

    correcting edit and delete function code

--- src/pages/userProfile/UserProfileModal.jsx
+++ src/pages/userProfile/UserProfileModal.jsx
@@ -1,55 +1,62 @@
-import React from 'react'
-import { useState } from 'react';
-import GlobalApi from '../../Utils/GlobalApi';
-import UserProfilePropertyCards from './UserProfilePropertyCards';
+import React from "react";
+import { useState } from "react";
+import GlobalApi from "../../Utils/GlobalApi";
+import UserProfilePropertyCards from "./UserProfilePropertyCards";
 
 const UserProfileModal = ({ id, postedHouses }) => {
-      const [properties, setProperties] = useState([]);
-    
-      const fetchProperties = async () => {
-        try {
-          if (!postedHouses || postedHouses.length === 0) return;
-            const responses = await GlobalApi.getProfileByClerkId(id)
-            console.log(responses.data)
-            setProperties(responses.data)
-        } catch (error) {
-          console.error("Error fetching properties:", error);
-        }
-      };
+  const [properties, setProperties] = useState([]);
+
+  const fetchProperties = async () => {
+    try {
+      if (!postedHouses || postedHouses.length === 0) return;
+      const responses = await GlobalApi.getProfileByClerkId(id);
+      console.log(responses.data);
+      setProperties(responses.data);
+    } catch (error) {
+      console.error("Error fetching properties:", error);
+    }
+  };
   return (
     <div>
       <div>
-      <button className="btn btn-primary mt-2" onClick={() => {
-        fetchProperties();
-        document.getElementById('my_modal_4').showModal();
-      }}>
-        View All
-      </button>
-      
-      <dialog id="my_modal_4" className="modal">
-        <div className="modal-box w-11/12 max-w-5xl"> 
-          <h3 className="font-bold text-lg">Your Properties</h3>
+        <button
+          className="btn btn-primary mt-2"
+          onClick={() => {
+            fetchProperties();
+            document.getElementById("my_modal_4").showModal();
+          }}
+        >
+          View All
+        </button>
 
-                  <div className='p-4 flex gap-4'>
-                  {properties?.postedHouses?.length > 0 ?
-                      (properties?.postedHouses?.map((item, index) => {
-                          return (<div className='' key={index}> <UserProfilePropertyCards item={item} /> </div>)
-                    }))
-           : (
-            <p>No properties found.</p>
-          )}
+        <dialog id="my_modal_4" className="modal">
+          <div className="modal-box w-11/12 max-w-5xl">
+            <h3 className="font-bold text-lg">Your Properties</h3>
 
-                  </div>
-          <div className="modal-action">
-            <form method="dialog">
-              <button className="btn">Close</button>
-            </form>
+            <div className="p-4 flex gap-4">
+              {properties?.postedHouses?.length > 0 ? (
+                properties?.postedHouses?.map((item, index) => {
+                  return (
+                    <div className="" key={index}>
+                      {" "}
+                      <UserProfilePropertyCards item={item} />{" "}
+                    </div>
+                  );
+                })
+              ) : (
+                <p>No properties found.</p>
+              )}
+            </div>
+            <div className="modal-action">
+              <form method="dialog">
+                <button className="btn">Close</button>
+              </form>
+            </div>
           </div>
-        </div>
-      </dialog>
+        </dialog>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default UserProfileModal
+export default UserProfileModal;
