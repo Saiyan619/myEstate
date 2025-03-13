095dbbb08346c0162acdaeed664deeb8af2adb79
olaniyi <arokoyueb11@gmail.com>
2025-03-13 04:01:37 +0100

    add auth wrapper on homepage

--- src/Utils/GlobalApi.js
+++ src/Utils/GlobalApi.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 // Base Url host connecting frontend to the backend (Making use of the global Api method)
 const axiosClient = axios.create({
-    baseURL: 'http://localhost:5000/'
+    baseURL: import.meta.env.PUBLIC_API_URL
+    // baseURL: 'http://localhost:5000/'
 });
 
 
